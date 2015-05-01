9e8065e268517ff7d3fb2e34e2b5242210f501ae
张义 <chinayi@163.com>
2015-05-01 23:14:23 +0800

    提交源码

---
+++ cn.hpapa.bkl.core/src/main/java/cn/hpapa/bkl/cn/hpapa/bkl/core/App.java
@@ -0,0 +1,13 @@
+package cn.hpapa.bkl.cn.hpapa.bkl.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
