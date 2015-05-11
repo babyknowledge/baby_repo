2ac359075edc9f1fd4a30c60eb21d5d2cb0cc142
张义 <chinayi@163.com>
2015-05-11 17:38:10 +0800

    add study project

---
+++ cn.hpapa.bkl.study/src/main/java/cn/hpapa/bkl/study/ConsoleIn.java
@@ -0,0 +1,29 @@
+package cn.hpapa.bkl.study;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Hello world!
+ * 控制台输入
+ */
+public class ConsoleIn 
+{
+    public static void main( String[] args )
+ {
+		try {
+			InputStreamReader isr = new InputStreamReader(System.in);
+			BufferedReader br = new BufferedReader(isr);
+			// 请输入第一个数
+			System.out.println("请输入第一个数...");
+			String a1 = br.readLine();
+			// 请输入第二个数
+			System.out.println("请输入第二个数...");
+			String a2 = br.readLine();
+
+			System.out.println(a1 + "\n1" + a2);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
